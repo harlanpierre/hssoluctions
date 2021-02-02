93933048ff9b2aba80bc371a712d9eccacdff7e3
Harlan Pierre <harlan.evron@gmail.com>
2021-02-01 22:06:43 -0300

    Projeto Criado

---
+++ src/main/java/com/hsimportados/hssoluctions/HssoluctionsApplication.java
@@ -0,0 +1,13 @@
+package com.hsimportados.hssoluctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HssoluctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HssoluctionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsimportados/hssoluctions/HssoluctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsimportados.hssoluctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HssoluctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
